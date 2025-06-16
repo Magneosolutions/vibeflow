ed188f9bf95e7ec0a3759f33d7e72cddc19f01e8
Grace Esteban <mesteban@ccsf.edu>
2025-06-15 18:06:07 -0700

    Update firebaseConfig.ts
    deleted api key

--- vibeflow-frontend/src/firebaseConfig.ts
+++ vibeflow-frontend/src/firebaseConfig.ts
@@ -3,10 +3,10 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIR4WJthrMjy6SH4CkHc7PHcKwHYFSSQU",
-  authDomain: "my-app-project-461822.firebaseapp.com",
-  projectId: "my-app-project-461822",
-  storageBucket: "my-app-project-461822.firebasestorage.app",
+  apiKey:your api key,
+  authDomain: "my-app-project.firebaseapp.com",
+  projectId: "my-app-project",
+  storageBucket: "my-app-project.firebasestorage.app",
   messagingSenderId: "679820915121",
   appId: "1:679820915121:web:ff68938b52ab90d1fc4d90"
 };
